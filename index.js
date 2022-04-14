3bf128b2c0d380ea4763d5b29d89e162cc246717
Md-Sefat-Alam <md.sefatalam@gmail.com>
2022-04-15 03:12:03 +0600

    cart, order place, manage all orders, my orders

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -23,10 +23,115 @@ app.post("/register", (req, res) => {
     // client.close();
   });
 });
+app.get("/isadmin/:email", (req, res) => {
+  const email = req.params.email;
+  client.connect(async (err) => {
+    const collection = client.db("action_camera").collection("usersData");
+    const result = await collection.findOne({ email: email });
+    res.send(result);
+    // client.close();
+  });
+});
+app.post("/orderplace", (req, res) => {
+  client.connect(async (err) => {
+    const collection = client.db("action_camera").collection("buyRequests");
+    const result = await collection.insertOne(req.body);
+    res.send(result);
+    // client.close();
+  });
+});
+app.get("/myorders/:email", (req, res) => {
+  const email = req.params.email;
+  client.connect(async (err) => {
+    const collection = client.db("action_camera").collection("buyRequests");
+    const result = collection.find({ email: email });
+    const myOrders = await result.toArray();
+    res.send(myOrders);
+    // client.close();
+  });
+});
+app.put("/order-make-approved/:id", (req, res) => {
+  const id = req.params.id;
+  client.connect(async (err) => {
+    const collection = client.db("action_camera").collection("buyRequests");
+    const filter = { _id: ObjectId(id) };
+    const options = { upsert: true };
+    const updateStatus = {
+      $set: {
+        status: "approved",
+      },
+    };
+    const result = await collection.updateOne(filter, updateStatus, options);
+    res.send(result);
+    // client.close();
+  });
+});
+app.get("/all-orders", (req, res) => {
+  const email = req.params.email;
+  client.connect(async (err) => {
+    const collection = client.db("action_camera").collection("buyRequests");
+    const result = collection.find({});
+    const allOrders = await result.toArray();
+    res.send(allOrders);
+    // client.close();
+  });
+});
+app.delete("/myorders-remove-one/:id", (req, res) => {
+  const id = req.params.id;
+  client.connect(async (err) => {
+    const collection = client.db("action_camera").collection("buyRequests");
+    const result = await collection.deleteOne({ _id: ObjectId(id) });
+    res.send(result);
+    // client.close();
+  });
+});
 app.post("/add_a_product", (req, res) => {
   client.connect((err) => {
     const collection = client.db("action_camera").collection("products");
     const result = collection.insertOne(req.body);
+    res.send(result);
+    // client.close();
+  });
+});
+app.post("/addToCart", (req, res) => {
+  const newCartData = req.body;
+  client.connect(async (err) => {
+    const collection = client.db("action_camera").collection("cart");
+    const result = await collection.insertOne(newCartData);
+    res.send(result);
+    // client.close();
+  });
+});
+app.get("/getToCart/:email", (req, res) => {
+  const email = req.params.email;
+  const id = req.query.id;
+  console.log(email, id);
+  client.connect(async (err) => {
+    const collection = client.db("action_camera").collection("cart");
+    const result = await collection.findOne({ email: email, id: id });
+    console.log(result);
+    res.send(result);
+    // client.close();
+  });
+});
+app.get("/singleUserCartData/:email", (req, res) => {
+  const email = req.params.email;
+  client.connect(async (err) => {
+    const collection = client.db("action_camera").collection("cart");
+    const result = collection.find({ email: email });
+    const singleUserAllCartData = await result.toArray();
+    console.log(singleUserAllCartData);
+    res.send(singleUserAllCartData);
+    // client.close();
+  });
+});
+app.delete("/singleUserCartDataDelete/:email", (req, res) => {
+  const email = req.params.email;
+  console.log(email);
+  client.connect(async (err) => {
+    const collection = client.db("action_camera").collection("cart");
+    const result = await collection.deleteMany({ email: email });
+    console.log(result);
     res.send(result);
     // client.close();
   });
